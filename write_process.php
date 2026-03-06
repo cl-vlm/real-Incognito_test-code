69e3bc63305c38f2b84f269a98fdb91532b260e3
cl-vlm <daseul2006@ajou.ac.kr>
2026-03-06 17:40:23 +0900

    Update write_process.php

--- write_process.php
+++ write_process.php
@@ -1,26 +1,15 @@
 <?php
-session_start();
 include "db.php";
-
-// 로그인 체크 (최소한의 보안)
-if(!isset($_SESSION['userid'])) {
-    die("<script>alert('로그인이 필요합니다.'); location.href='index.html';</script>");
-}
 
 $title = $_POST['title'];
 $content = $_POST['content'];
-$author = $_SESSION['userid'];
+$author = "Guest"; // 로그인을 없앴으므로 Guest로 고정
 
-/* 🚩 워게임 장치: 가짜 방어 로직 
-   'script'라는 단어를 'x-script'로 치환합니다. 
-   이로 인해 일반적인 XSS 공격은 막히지만, <a>나 <div> 태그를 이용한 
-   DOM Clobbering 공격은 그대로 허용하게 됩니다.
-*/
+// 🚩 워게임 장치: script 태그만 치환 (DOM Clobbering은 허용)
 $content = str_ireplace("script", "x-script", $content);
 
-// DB에 저장 (내용을 그대로 저장해야 취약점이 발생함)
-$sql = "INSERT INTO board (title, author, content) VALUES ('$title', '$author', '$content')";
-mysqli_query($conn, $sql);
+$stmt = $conn->prepare("INSERT INTO board (title, author, content) VALUES (?, ?, ?)");
+$stmt->execute([$title, $author, $content]);
 
-echo "<script>alert('글이 등록되었습니다.'); location.href='main.php';</script>";
+echo "<script>alert('등록 완료!'); location.href='main.php';</script>";
 ?>
