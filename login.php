8656b916456b1a16c6e2c044b104d672d71014dd
cl-vlm <daseul2006@ajou.ac.kr>
2026-03-06 17:57:07 +0900

    Update login.php

--- login.php
+++ login.php
@@ -1,15 +1,16 @@
 <?php
-session_start(); // 세션 시작
+session_start();
 include "db.php";
 
 $id = $_POST['username'];
 $pw = $_POST['password'];
 
-$sql = "SELECT * FROM users WHERE username='$id' AND password='$pw'";
-$result = mysqli_query($conn, $sql);
+$stmt = $conn->prepare("SELECT * FROM users WHERE username = ? AND password = ?");
+$stmt->execute([$id, $pw]);
+$user = $stmt->fetch();
 
-if (mysqli_num_rows($result) > 0) {
-    $_SESSION['userid'] = $id; // 세션에 ID 저장
+if ($user) {
+    $_SESSION['userid'] = $id;
     echo "<script>alert('로그인 성공!'); location.href='main.php';</script>";
 } else {
     echo "<script>alert('아이디 또는 비밀번호가 틀렸습니다.'); history.back();</script>";
