cff47d154c21ae2faafdff58ebbedd16d9fe20e3
cl-vlm <daseul2006@ajou.ac.kr>
2026-03-06 17:57:45 +0900

    Update write_process.php

--- write_process.php
+++ write_process.php
@@ -1,15 +1,18 @@
 <?php
+session_start();
 include "db.php";
+
+if(!isset($_SESSION['userid'])) die("로그인이 필요합니다.");
 
 $title = $_POST['title'];
 $content = $_POST['content'];
-$author = "Guest"; // 로그인을 없앴으므로 Guest로 고정
+$author = $_SESSION['userid'];
 
-// 🚩 워게임 장치: script 태그만 치환 (DOM Clobbering은 허용)
+// 워게임 장치: script 단어만 무력화
 $content = str_ireplace("script", "x-script", $content);
 
 $stmt = $conn->prepare("INSERT INTO board (title, author, content) VALUES (?, ?, ?)");
 $stmt->execute([$title, $author, $content]);
 
-echo "<script>alert('등록 완료!'); location.href='main.php';</script>";
+echo "<script>alert('등록되었습니다.'); location.href='main.php';</script>";
 ?>
