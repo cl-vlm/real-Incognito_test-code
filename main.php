c34d5c4b32701c511810db2dcd2282f3895af427
cl-vlm <daseul2006@ajou.ac.kr>
2026-03-06 17:57:25 +0900

    Update main.php

--- main.php
+++ main.php
@@ -3,13 +3,34 @@ include "db.php";
 $stmt = $conn->query("SELECT * FROM board ORDER BY num DESC");
 $rows = $stmt->fetchAll();
 ?>
-<tbody>
-    <?php foreach($rows as $row) { ?>
-    <tr>
-        <td><?= $row['num'] ?></td>
-        <td style="text-align: left;"><a href="view.php?num=<?= $row['num'] ?>"><?= htmlspecialchars($row['title']) ?></a></td>
-        <td><?= htmlspecialchars($row['author']) ?></td>
-        <td><a href="delete.php?num=<?= $row['num'] ?>" style="color: red;">삭제</a></td>
-    </tr>
-    <?php } ?>
-</tbody>
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+    <meta charset="UTF-8">
+    <title>INCOGNITO BOARD</title>
+    <link rel="stylesheet" href="index.css">
+</head>
+<body>
+    <div style="padding:50px; max-width:900px; margin:auto;">
+        <h2>INCOGNITO PROJECT BOARD</h2>
+        <table style="width:100%; border-collapse: collapse;">
+            <thead>
+                <tr style="background:#4EA685; color:white;">
+                    <th>번호</th><th>제목</th><th>작성자</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php foreach($rows as $row) { ?>
+                <tr>
+                    <td style="text-align:center;"><?= $row['num'] ?></td>
+                    <td><a href="view.php?num=<?= $row['num'] ?>"><?= htmlspecialchars($row['title']) ?></a></td>
+                    <td style="text-align:center;"><?= htmlspecialchars($row['author']) ?></td>
+                </tr>
+                <?php } ?>
+            </tbody>
+        </table>
+        <br>
+        <button onclick="location.href='write.php'">새 글 쓰기</button>
+    </div>
+</body>
+</html>
