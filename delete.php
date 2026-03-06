328ab2e5b58db2eb48cb5471782b670e36336067
cl-vlm <daseul2006@ajou.ac.kr>
2026-03-06 18:30:37 +0900

    Update delete.php

--- delete.php
+++ delete.php
@@ -2,7 +2,11 @@
 include "db.php";
 $num = $_GET['num'];
 
-$sql = "DELETE FROM board WHERE num = $num";
-mysqli_query($conn, $sql);
-echo "<script>alert('삭제되었습니다.'); location.href='main.php';</script>";
+// PDO 방식으로 수정
+$stmt = $conn->prepare("DELETE FROM board WHERE num = ?");
+if ($stmt->execute([$num])) {
+    echo "<script>alert('삭제되었습니다.'); location.href='main.php';</script>";
+} else {
+    echo "삭제 실패";
+}
 ?>
